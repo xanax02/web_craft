a7160e8885e9e8ee6a644f654bb128cbc952908e
abhay <abhaytxanax@gmail.com>
2026-02-18 16:33:50 +0530

    updated middlware and routes

---
+++ src/lib/routesPermissions.tsx
@@ -0,0 +1,11 @@
+export const byPassRoutes = [
+  "/api/auth/signin",
+  "/api/auth/signup",
+  "/convex/(.*)",
+  "/api/inngest/(.*)",
+  "/api/polar/webhook",
+];
+
+export const clientAuthRoutes = ["auth/signin", "auth/signup"];
+
+export const protectedRoutes = ["/client/(.*)"];
--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,8 +1,39 @@
-import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server";
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+import {
+  byPassRoutes,
+  clientAuthRoutes,
+  protectedRoutes,
+} from "./lib/routesPermissions";
 
-const byPassRoutes = ["/auth/signin", "/auth/signup"];
+const byPassMatcher = createRouteMatcher(byPassRoutes);
+const clientAuthMatcher = createRouteMatcher(clientAuthRoutes);
+const protectedMatcher = createRouteMatcher(protectedRoutes);
 
-export default convexAuthNextjsMiddleware();
+export default convexAuthNextjsMiddleware(
+  async (req, { convexAuth }) => {
+    if (byPassMatcher(req)) return;
+
+    const isAuthenticated = await convexAuth.isAuthenticated();
+
+    if (clientAuthMatcher(req) && isAuthenticated) {
+      //needs to be changed if redirect to some other page.
+      return nextjsMiddlewareRedirect(req, "/");
+    }
+
+    if (protectedMatcher(req) && !isAuthenticated) {
+      return nextjsMiddlewareRedirect(req, "/auth/signin");
+    }
+
+    return;
+  },
+  {
+    cookieConfig: { maxAge: 60 * 60 * 24 * 30 },
+  },
+);
 
 export const config = {
   // The following matcher runs middleware on all routes
