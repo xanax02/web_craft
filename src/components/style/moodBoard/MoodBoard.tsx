debb3123138d8ddcdc64dd6808a1ff800739e875
abhay <abhaytxanax@gmail.com>
2026-03-17 17:27:34 +0530

    implemented drag feature for MoodBoard

--- src/components/style/moodBoard/MoodBoard.tsx
+++ src/components/style/moodBoard/MoodBoard.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { MoodBoardImages, useMoodBoard } from "@/hooks/useStyles";
+import { cn } from "@/lib/utils";
+import ImagesBoard from "./ImagesBoard";
 
 type Props = {
   guideImages: MoodBoardImages[];
@@ -16,11 +19,61 @@ export default function MoodBoard({ guideImages }: Props) {
   } = useMoodBoard(guideImages);
 
   return (
-    <div className="flex flex-col gap-10">
-      <div className="relative border-2 border-dashed rounded-3xl p-12 text-center transition-all duration-200 min-h-[500px] flex items-center justify-center"></div>
+    <div className="mx-4 flex flex-col gap-10">
+      <div
+        className={cn(
+          "relative border-2 border-dashed rounded-3xl p-12 text-center transition-all duration-200 min-h-[500px] flex items-center justify-center",
+          dragActive
+            ? "border-primary bg-primary/5 scale-[1.02]"
+            : "border-border/50 hover:border-border",
+        )}
+        onDragEnter={handleDrag}
+        onDragLeave={handleDrag}
+        onDragOver={handleDrag}
+        onDrop={handleDrag}
+      >
+        <div className="absolute inset-0 opacity-5">
+          <div className="w-full h-full bg-linear-to-br from-primary/20 to-transparent rounded-3xl"></div>
+        </div>
 
-      <div className="absolute inset-0 opacity-5">
-        <div className="w-full h-full bg-linear-to-br from-primary/20 to-transparent rounded-3xl"></div>
+        {images.length > 0 && (
+          <>
+            <div className="lg:hidden absolute inset-0, flex items-center justify-center">
+              <div className="relative">
+                {images.map((image, index) => {
+                  const seed = image.id
+                    .split("")
+                    .reduce((a, b) => a + b.charCodeAt(0), 0);
+
+                  const random1 = ((seed * 9301 + 49297) % 233280) / 233280;
+                  const random2 =
+                    (((seed + 1) * 9301 + 49297) % 233280) / 233280;
+                  const random3 =
+                    (((seed + 2) * 9301 + 49297) % 233280) / 233280;
+
+                  const rotation = (random1 - 0.5) * 20;
+                  const xOffset = (random2 - 0.5) * 40;
+                  const yOffset = (random3 - 0.5) * 30;
+
+                  return (
+                    <ImagesBoard
+                      key={`mobile-${image.id}`}
+                      image={image}
+                      removeImage={removeImage}
+                      xOffset={xOffset}
+                      yOffset={yOffset}
+                      rotation={rotation}
+                      zIndex={index + 1}
+                      marginLeft="-80px"
+                      marginTop="-96px
+                      "
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
